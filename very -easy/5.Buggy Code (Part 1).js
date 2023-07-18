b1418275dbbb95040987a08cbe62b505f77d6394
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-07-18 18:51:38 +0530

    Buggy Code (Part 1)

---
+++ very -easy/5.Buggy Code (Part 1).js
@@ -0,0 +1,10 @@
+function cubes() {
+  //    return arguments[0] **3
+  // return arguments[0].
+  return Math.pow(arguments[0], 3)
+}
+console.log(cubes(3))
+
+console.log(cubes(5))
+
+console.log(cubes(10))
