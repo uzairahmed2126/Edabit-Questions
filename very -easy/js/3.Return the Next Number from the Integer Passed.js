82d33b220786b88bf64a51cbb3e52c63f964d35a
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-07-18 17:19:11 +0530

    return the next number from the integer passed

---
+++ very -easy/js/3.Return the Next Number from the Integer Passed.js
@@ -0,0 +1,11 @@
+function addition(num) {
+  // return arguments[0]+1
+  // return ++num
+  // return Number(num+1)
+  if (Number == Number) {
+    return ++num
+  }
+}
+console.log(addition(0))
+console.log(addition(9))
+console.log(addition(-3))
