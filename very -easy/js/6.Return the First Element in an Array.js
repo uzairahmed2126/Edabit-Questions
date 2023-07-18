11a26225fcef5d37080b93ac8f00f27fbcc2d206
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-07-18 19:08:09 +0530

    Return the First Element in an Array

---
+++ very -easy/js/6.Return the First Element in an Array.js
@@ -0,0 +1,9 @@
+function getFirstValue(firstValue){
+    // return arguments[0][0]
+    // return firstValue[0]
+    // return Number(arguments[0][0])
+    return arguments[0][0]
+}
+console.log(getFirstValue([1, 2, 3]))
+console.log(getFirstValue([80, 5, 100]))
+console.log(getFirstValue([-500, 0, 50]))
