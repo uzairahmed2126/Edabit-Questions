f5da2e825918f59c6bec51caadd88e84b025377b
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-07-18 17:30:43 +0530

    Area of a Triangles

---
+++ very -easy/4.Area of a Triangle.js
@@ -0,0 +1,7 @@
+function triArea(base,height) {
+    return (arguments[0] * arguments[1]) / 2
+    // return (base * height) / 2
+}
+console.log(triArea(3, 2))
+console.log(triArea(7, 4))
+console.log(triArea(10, 10))
