d2e922bccfd5f6e07edcf2e960bd0e60938503ed
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-07-19 18:49:52 +0530

    convert hour into seconds

---
+++ very -easy/js/8.Convert Hours into Seconds.js
@@ -0,0 +1,7 @@
+function howManySeconds(hours){
+    // return 3600*hours
+    return 3600*arguments[0]
+}
+console.log(howManySeconds(2))
+console.log(howManySeconds(10))
+console.log(howManySeconds(24))
