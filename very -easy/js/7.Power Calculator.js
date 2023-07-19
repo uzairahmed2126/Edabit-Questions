eca06a80e6683cbb5a42400006506c0644b67f3d
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-07-19 18:30:10 +0530

    power calculate

---
+++ very -easy/js/7.Power Calculator.js
@@ -0,0 +1,7 @@
+function circuitPower(voltage,current){
+    // return arguments[0] * arguments[1]
+    return voltage*current
+}
+console.log(circuitPower(230, 10))
+console.log(circuitPower(110, 3))
+console.log(circuitPower(480, 20))
