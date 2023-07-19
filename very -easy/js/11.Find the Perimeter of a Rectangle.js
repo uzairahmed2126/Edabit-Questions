7caabd36692b8a88b41e14e843406e2c81293578
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-07-19 21:13:51 +0530

    Find the Perimeter of a Rectangle

---
+++ very -easy/js/11.Find the Perimeter of a Rectangle.js
@@ -0,0 +1,7 @@
+function findPerimeter(lenght,width){
+// return 2*(arguments[0] + arguments[1])
+return 2*(lenght+width)
+}
+console.log(findPerimeter(6, 7))
+console.log(findPerimeter(20, 10))
+console.log(findPerimeter(2, 9))
