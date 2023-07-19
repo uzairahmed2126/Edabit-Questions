da976afea8074e629f04c9b43630fd8b0c548790
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-07-19 22:47:06 +0530

    return something

---
+++ very -easy/js/12.Return Something to Me!.js
@@ -0,0 +1,14 @@
+function giveMeSomething(string) {
+//   return 'Something '+string
+// return concat('something')
+// let concat =  string.concat('Something')
+// if(String(string).length){
+//     return 'Something '+string
+// }
+// return 
+return 'Something '.concat(string)
+
+}
+console.log(giveMeSomething('is better than nothing'))
+console.log(giveMeSomething('Bob Jane'))
+console.log(giveMeSomething('something'))
