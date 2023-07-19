98d4db8e9e4b8fa890ec56814f4b1872bfa1d1f2
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-07-19 23:25:09 +0530

    return something

--- very -easy/js/12.Return Something to Me!.js
+++ very -easy/js/12.Return Something to Me!.js
@@ -1,14 +1,13 @@
 function giveMeSomething(string) {
-//   return 'Something '+string
-// return concat('something')
-// let concat =  string.concat('Something')
-// if(String(string).length){
-//     return 'Something '+string
-// }
-// return 
-return 'Something '.concat(string)
-
+  //return 'Something '+string
+  //return 'Something '.concat(string)
+  //return `Something ${string}`
+//   return 'Something '+string.slice()
+// return 'Something '+string.substring(0)
+// return 'Something '+string.startsWith()
+return  string.replace(/^/,'Something ')
 }
 console.log(giveMeSomething('is better than nothing'))
 console.log(giveMeSomething('Bob Jane'))
 console.log(giveMeSomething('something'))
+
