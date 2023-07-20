857879a45bd6337c8c65b2d1245b5db74cb6372e
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-07-20 14:48:01 +0530

    basketball points

---
+++ very -easy/js/16.Basketball Points.js
@@ -0,0 +1,7 @@
+function points(score1,score2) {
+    // return arguments[0]*2+arguments[1]*3
+    return (score1*2+score2*3)
+}
+console.log(points(1, 1))
+console.log(points(7, 5))
+console.log(points(38, 8))
