45a7e8aab119f103088a0238894d22beb5bbd934
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-07-20 14:52:51 +0530

    basic variable assignment

---
+++ very -easy/js/17.Basic Variable Assignment.js
@@ -0,0 +1,8 @@
+function nameString(str){
+    // return arguments[0].concat('Edabit')
+    let result = str+'Edabit'
+    return result
+}
+console.log(nameString('Mubashir'))
+console.log(nameString('Matt'))
+console.log(nameString('javaScript'))
