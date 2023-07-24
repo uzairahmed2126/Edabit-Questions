9e98ea8a8a460133cac6d84ae204b2fe879ab786
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-07-24 23:02:48 +0530

    .

--- very -easy/js/21.The Farm Problem.js
+++ very -easy/js/21.The Farm Problem.js
@@ -1,5 +1,9 @@
-function animals(){
-    return arguments[0]*2+ arguments[1]*4+arguments[2]*4
+function animals(chickens, cows, pigs) {
+  //   return chickens[0] * 2 + cows[1] * 4 + pigs[2] * 4
+  const legs = [chickens * 2, cows * 4, pigs * 4]
+  return legs.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,0
+  )
 }
 console.log(animals(2, 3, 5))
 console.log(animals(1, 2, 3))
