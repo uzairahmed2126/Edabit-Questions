360eb12ede0e184fc38710b695c4718dcecc2782
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-07-25 19:07:00 +0530

    Football Points

---
+++ very -easy/js/24.Football Points.js
@@ -0,0 +1,6 @@
+function footballPoints(wins,draws,losses){
+    return (wins*3)+(draws*1)+(losses*0)
+}
+console.log(footballPoints(3, 4, 2))
+console.log(footballPoints(5, 0, 2))
+console.log(footballPoints(0, 0, 1))
