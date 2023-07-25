750237f63967ecb92a9434e376ae457efce10284
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-07-25 20:38:40 +0530

    profitable gamble

---
+++ very -easy/js/26.Profitable Gamble.js
@@ -0,0 +1,6 @@
+function profitableGamble(prob,prize,pay){
+    return (prob*prize)>pay
+}
+console.log(profitableGamble(0.2, 50, 9))
+console.log(profitableGamble(0.9, 1, 2))
+console.log(profitableGamble(0.9, 3, 2))
