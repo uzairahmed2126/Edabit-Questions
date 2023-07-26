a139099d2cd6bf18c135a9ead9de940c890eed20
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-07-26 15:43:36 +0530

    buggy code(part 4)

---
+++ very -easy/js/32.Buggy Code (Part 4).js
@@ -0,0 +1,11 @@
+function greeting(name){
+    return name==='Mubashir'? `Hello, my Love!`: `Hello, ${name}!`
+    // if(name==='Mubashir'){
+    //     return 'Hello, my love!'
+    // }else{
+    //     return `Hello, ${name} !`
+    // }
+}
+console.log(greeting('Matt'))
+console.log(greeting('Helen'))
+console.log(greeting('Mubashir'))
