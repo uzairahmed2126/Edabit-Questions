5ea2368abdca1b5c5f6701d75ab31f59a91e5e49
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-07-26 22:53:05 +0530

    .

--- very -easy/js/34.Let's Fuel Up!.js
+++ very -easy/js/34.Let's Fuel Up!.js
@@ -1,7 +1,7 @@
-function calculateFuel(distance){
-    //  return (distance*10) < 100 ? 100 : distance*10
-    const  requiredFuel = distance*10
-    return Math.max(requiredFuel,100)
+function calculateFuel(distance) {
+  //  return (distance*10) < 100 ? 100 : distance*10
+  const requiredFuel = distance * 10
+  return Math.max(requiredFuel, 100)
 }
 
 console.log(calculateFuel(15))
