28c64d7956d90a99e42f5667fc42e550a9ce728a
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-07-26 23:19:20 +0530

    Compare String by count of characters

---
+++ very -easy/js/37.Compare Strings by Count of Characters.js
@@ -0,0 +1,8 @@
+function comp(str1, str2) {
+  return (str1.length) === (str2.length) ? true : false
+}
+console.log(comp('AB', 'CD'))
+
+console.log(comp('ABC', 'DE'))
+
+console.log(comp('hello', 'edabit'))
