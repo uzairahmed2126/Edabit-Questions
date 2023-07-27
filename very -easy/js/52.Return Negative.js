8f879dca4cb0d849a47194f1f4d76efeb905af59
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-07-28 02:21:48 +0530

    Return Negative

---
+++ very -easy/js/52.Return Negative.js
@@ -0,0 +1,14 @@
+function returnNegative(n){ 
+    if(n>0){
+        return -n
+    }else{
+        return n
+    }
+}
+console.log(returnNegative(4))
+
+console.log(returnNegative(15))
+
+console.log(returnNegative(-4))
+
+console.log(returnNegative(0))
