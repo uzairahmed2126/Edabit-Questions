2a43384e0240e5675b0b05fc86b1e81c24289922
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-07-27 15:11:37 +0530

    Concatenate First and Last Name into One String

---
+++ very -easy/js/46.Concatenate First and Last Name into One String.js
@@ -0,0 +1,10 @@
+function concatName(firsName,lastName){
+    return (firsName)+','+(lastName)
+    // return String().concat(firsName+(',')+lastName)
+}
+console.log(concatName("First", "Last") )
+
+console.log(concatName("John", "Doe") )
+
+console.log(concatName("Mary", "Jane") )
+
