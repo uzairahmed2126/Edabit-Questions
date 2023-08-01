efed54fa65a9e27b331896a5d26e9c57a4d0299b
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-08-01 22:41:28 +0530

    Find the index part 2 by loop and method

---
+++ very -easy/js/62.Find the Index (Part 1).js
@@ -0,0 +1,15 @@
+function search(num,ind){
+    let a ;
+    for(let i=0 ; i<num.length;i++){
+        // a = num[i]===ind ? i:-1
+        // if(num[i]===ind){
+        //     return i
+        // }
+    }
+    return num.indexOf(ind)
+}
+console.log(search([1, 5, 3], 5) )
+
+console.log(search([9, 8, 3], 3) )
+
+console.log(search([1, 2, 3], 4) )
