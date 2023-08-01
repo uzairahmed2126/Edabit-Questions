8cc390953ac2606978df8faf440c5f8a6b1a04e2
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-08-01 19:23:05 +0530

    To the power of by loop

---
+++ very -easy/js/60.To the Power of.js
@@ -0,0 +1,12 @@
+function calculateExponent(exponent,base){
+    let total;
+    for (let index = 0; index < 2; index++) {
+        total = exponent**base
+    }
+    return total
+}
+console.log(calculateExponent(5, 5) )
+
+console.log(calculateExponent(10, 10) )
+
+console.log(calculateExponent(3, 3) )
