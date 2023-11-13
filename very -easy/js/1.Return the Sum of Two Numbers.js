b428e6ca2648638b9e2a8b200531874398fa7e27
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-11-14 01:10:58 +0530

    return the sum of two numbers

--- very -easy/js/1.Return the Sum of Two Numbers.js
+++ very -easy/js/1.Return the Sum of Two Numbers.js
@@ -1,13 +1,17 @@
-function addition(num1,num2){
-    if(Number.isInteger){
-         return num1+num2
-    }
+function addition(num1, num2) {
+    // if(Number.isInteger){
+    //      return num1+num2
+    // }
     // return num1 + num2
     // return arguments[0] + arguments[1]
     // return Number(+num1+num2)
     // return Number.isInteger(num1,num2)
-
+    let a = 0;
+    for (let i = 1; i <= addition.length; i++) {
+        a = num1 + +num2;
+    }
+    return a;
 }
 console.log(addition(3, 2))
-console.log(addition(-3, -6)) 
-console.log(addition(7, 3))
+console.log(addition(-3, -6))
+console.log(addition(7, '3'))
