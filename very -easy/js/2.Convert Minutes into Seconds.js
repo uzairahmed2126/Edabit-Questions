b6b51d57dda381669573a6e9fe6dad3772819a44
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-11-14 01:16:16 +0530

    convert minutes into seconds

--- very -easy/js/2.Convert Minutes into Seconds.js
+++ very -easy/js/2.Convert Minutes into Seconds.js
@@ -1,7 +1,10 @@
-function convert() {
-//  return arguments[0] * 60
-return Number(arguments[0] * 60)
-}
+function convert(minutes) {
+    for (let i = 1; i <= minutes; i++) {
+        return minutes * 60;
+    };
+    //  return arguments[0] * 60
+    // return Number(arguments[0] * 60)
+};
 
 console.log(convert(5))
 console.log(convert(3))
