d98d8ec7ed266f821812bde505a4a51193cbb20c
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-11-15 02:20:18 +0530

    arrayReverse

---
+++ meet/3.arrayReverse.js
@@ -0,0 +1,10 @@
+function arrreverse(arr) {
+    // let result = [];
+    // for (let i = arr.length-1; i >=0 ; i--) {
+    //     result.push(arr[i])
+    // }
+    // return result
+    return arr.reverse()
+
+}
+console.log(arrreverse(['uzair', 'fayyaz', 'juber']));
