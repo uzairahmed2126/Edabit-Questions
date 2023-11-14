866b50d7add3ec6a863889e75e086d4cf3aff88f
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-11-14 22:09:05 +0530

    .

--- very -easy/js/6.Return the First Element in an Array.js
+++ very -easy/js/6.Return the First Element in an Array.js
@@ -1,9 +1,22 @@
-function getFirstValue(firstValue){
+function getFirstValue(firstValue) {
     // return arguments[0][0]
     // return firstValue[0]
     // return Number(arguments[0][0])
-    return arguments[0][0]
+    // return arguments[0][0]
+    let a = 0;
+    for (let i = 0; i <= firstValue.length; i++) {
+        a = firstValue[0]
+    }
+    return a;
+    // for (let i = 0; i < firstValue.length; i++) {
+    //     return firstValue[i];
+    // }
+
+    // // Handle the case where the array is empty
+    // console.error("Array is empty.");
+    // return undefined;
 }
+
 console.log(getFirstValue([1, 2, 3]))
 console.log(getFirstValue([80, 5, 100]))
 console.log(getFirstValue([-500, 0, 50]))
