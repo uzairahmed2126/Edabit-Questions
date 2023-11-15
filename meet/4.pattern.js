cba3992e0a409e578f37f6a04e12dc86e159abc6
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-11-15 22:48:31 +0530

    .

---
+++ meet/4.pattern.js
@@ -0,0 +1,14 @@
+for (let i = 1; i <= 6; i++) {
+    let row = '';
+    for (let j = 1; j <= i; j++) {
+        row += j;
+    }
+    console.log(row);
+}
+
+// 1
+// 12
+// 123
+// 1234
+// 12345
+// 123456
