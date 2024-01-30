863ffce83e35ac3941a22b3c046a82082490aabc
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-01-31 00:58:24 +0530

    find the smallest and biggest number

--- easy/1.Find the Smallest and Biggest Numbers.js
+++ easy/1.Find the Smallest and Biggest Numbers.js
@@ -1,5 +1,17 @@
-function minMax(arr) {;
-    return [Math.min(...arr),Math.max(...arr)]
+function minMax(arr) {
+    // return [Math.min(...arr),Math.max(...arr)]
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - i - 1; j++) {
+          if (arr[j] > arr[j + 1]) {
+            let temp = arr[j];
+            arr[j] = arr[j + 1];
+            arr[j + 1] = temp;
+          }
+        }
+      }
+    
+      // After sorting, return [min, max]
+      return [arr[0], arr[arr.length - 1]];
 }
 console.log(minMax([1, 2, 3, 4, 5])); //➞ [1, 5]
 
