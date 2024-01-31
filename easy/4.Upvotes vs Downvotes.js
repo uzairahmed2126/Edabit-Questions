3141956076fb54ccedc662242b04f0defb01e9bf
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-01-31 14:18:47 +0530

    upvotes vs downvotes

--- easy/4.Upvotes vs Downvotes.js
+++ easy/4.Upvotes vs Downvotes.js
@@ -1,8 +1,11 @@
-function getVoteCount(obj) {
-    // return obj.upvotes - obj.downvotes
-    return Array(obj).map(function (item) {
-        return item.upvotes - item.downvotes;
-    });
+function getVoteCount(votes) {
+    // return votes.upvotes - votes.downvotes
+    // return Array(votes).map(function (item) {
+    //     return item.upvotes - item.downvotes;
+    // });
+    const { upvotes, downvotes } = votes; //this approach called distructing.
+    const voteCount = [upvotes, -downvotes].reduce((total, current) => total + current, 0);
+    return voteCount;
 };
 console.log(getVoteCount({ upvotes: 13, downvotes: 0 })); //➞ 13
 console.log(getVoteCount({ upvotes: 2, downvotes: 33 })); //➞ -31
