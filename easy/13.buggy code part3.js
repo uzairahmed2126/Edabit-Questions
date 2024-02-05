be478944a1666e2fa67943add74cd36a2f818e05
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-02-05 18:24:44 +0530

    buggy code part 3

---
+++ easy/13.buggy code part3.js
@@ -0,0 +1,13 @@
+function sumArray(arr) {
+    let result = 0;
+    // for (let i = 0; i < arr.length; i++) {
+    //     result += arr[i]
+    // }
+    // return result
+    // return arr.reduce(function (acc, cur) {
+    //     return acc + cur;
+    // });
+};
+console.log(sumArray([1, 2, 3, 4, 5])); //➞ 15
+console.log(sumArray([-1, 0, 1])); //➞ 0
+console.log(sumArray([0, 4, 8, 12])); //➞ 24
