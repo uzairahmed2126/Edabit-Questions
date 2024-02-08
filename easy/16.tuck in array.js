342ba24fa5afa218b5b05a976e8d11debf9731cf
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-02-08 14:16:21 +0530

    tuck in array

--- easy/16.tuck in array.js
+++ easy/16.tuck in array.js
@@ -1,7 +1,9 @@
 function tuckIn(arr1, arr2) {
-    return [arr1[0], ...arr2, arr1[1]];
+    // return [arr1[0], ...arr2, arr1[1]];
+    const uniqeArray = arr2.filter(function (item) { return !arr1.includes(item) });
+    return arr1.slice(0, 1).concat(uniqeArray, arr1.slice(1));
 }
-console.log(tuckIn([1, 10], [2, 3, 4, 5, 6, 7, 8, 9])); //➞ [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+console.log(tuckIn([1, 10], [2, 3, 4, 5, 6, 7, 8, 9, 10,10])); //➞ [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 console.log(tuckIn([15, 150], [45, 75, 35])); //➞ [15, 45, 75, 35, 150]
 
