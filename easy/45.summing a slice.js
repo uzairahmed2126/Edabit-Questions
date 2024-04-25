748c98b575f6a0ab9e5981840ad45b92a75f92a6
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-04-25 12:59:02 +0530

    summing

---
+++ easy/45.summing a slice.js
@@ -0,0 +1,17 @@
+function sliceSum(arr, n) {
+  //   let a = 0;
+  //   for (let i = 0; i < n; i++) {
+  //     a += arr[i];
+  //   }
+  //   return a;
+
+//   let a = arr.slice(0, n);
+//   let res = 0;
+//   let total = a.forEach((element) => {
+//     res += element;
+//   });
+//   return res;
+}
+console.log(sliceSum([1, 3, 2], 2)); //➞ 4
+console.log(sliceSum([4, 2, 5, 7], 4)); //➞ 18
+console.log(sliceSum([3, 6, 2], 0)); //➞ 0
