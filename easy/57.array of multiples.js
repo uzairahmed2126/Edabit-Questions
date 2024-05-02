3f79ec52193ec9291d6ca7c335649c6ec1d10075
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-05-02 13:11:54 +0530

    array of multiples

---
+++ easy/57.array of multiples.js
@@ -0,0 +1,19 @@
+// Array of Multiples
+// Create a function that takes two numbers as arguments (num, length) and returns an array of multiples of num until the array length reaches length.
+
+// Examples
+function arrayOfMultiples(num, length, currentLength = 1, result = []) {
+  // let arr = [];
+  // for (let i = 1; i <= length; i++) {
+  //   arr.push(num * i);
+  // }
+  // return arr;
+    if (currentLength > length) return result;
+    result.push(num * currentLength);
+    return arrayOfMultiples(num, length, currentLength + 1, result);
+}
+console.log(arrayOfMultiples(7, 15)); //➞ [7, 14, 21, 28, 35]
+console.log(arrayOfMultiples(12, 10)); //➞ [12, 24, 36, 48, 60, 72, 84, 96, 108, 120]
+console.log(arrayOfMultiples(17, 6)); //➞ [17, 34, 51, 68, 85, 102]
+// Notes
+// Notice that num is also included in the returned array.
