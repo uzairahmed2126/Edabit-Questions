235f57cb7af58b01f25034531548bfb87bd03e5f
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-05-02 18:50:20 +0530

    fix the error flattening an array

---
+++ easy/60.fix the error: flattening an array.js
@@ -0,0 +1,43 @@
+// Fix the Error: Flattening an Array
+// I'm trying to write a function to flatten an array of subarrays into one array. (Suppose I am unware there is a .flat() method in the Array prototype). In other words, I want to transform this: [[1, 2], [3, 4]] into [1, 2, 3, 4].
+
+// Here is my code:
+
+// function flatten(arr) {
+//   arr2 = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     arr2.concat(arr[i]);
+//   }
+//   return arr2;
+// }
+// But...it doesn't seem to be working! Fix my code so that it correctly flattens the array.
+
+// Examples
+function flatten(arr) {
+    let arr2 = [];
+    for (let i = 0; i < arr.length; i++) {
+      arr2 = arr2.concat(arr[i]);
+    }
+    return arr2;
+}
+console.log(
+  flatten([
+    [1, 2],
+    [3, 4],
+  ])
+); //➞ []
+// Expected: [1, 2, 3, 4]
+console.log(
+  flatten([
+    ["a", "b"],
+    ["c", "d"],
+  ])
+); //➞ []
+// Expected: ["a", "b", "c", "d"]
+console.log(
+  flatten([
+    [true, false],
+    [false, false],
+  ])
+); //➞ []
+// Expected: [true, false, false, false]
