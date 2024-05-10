0de1bb06994b8b66db8a533db822ec6039b59a48
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-05-10 16:20:33 +0530

    index multiplier

---
+++ easy/67.index multiplier.js
@@ -0,0 +1,23 @@
+// Index Multiplier
+// Return the sum of all items in an array, where each item is multiplied by its index (zero-based). For empty arrays, return 0.
+
+// Examples
+
+function indexMultiplier(arr) {
+  let count = 0;
+  //   for (let i = 0; i < arr.length; i++) {
+  // count += arr[i] * i;
+  //   }
+  //   return count;
+  //   arr.map((_, index) => (count += arr[index] * index));
+  //   return arr.reduce((val, cur, index) => val + cur * index, 0);
+  arr.map((_, index) => (count += arr[index] * index), 0);
+  return count;
+}
+console.log(indexMultiplier([1, 2, 3, 4, 5])); //➞ 40
+// (1*0 + 2*1 + 3*2 + 4*3 + 5*4)
+
+console.log(indexMultiplier([-3, 0, 8, -6])); //➞ -2
+// (-3*0 + 0*1 + 8*2 + -6*3)
+// Notes
+// All items in the array will be integers.
