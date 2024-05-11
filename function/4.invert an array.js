a4e67f0a30a2cb7815d4f6e98efd79969f153481
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-05-11 14:17:20 +0530

    invert an array

---
+++ function/4.invert an array.js
@@ -0,0 +1,17 @@
+// Invert an Array
+// Create a function that takes an array of numbers arr and returns an inverted array.
+
+// Examples
+function invertArray(arr) {
+  //   let invertedArr = [];
+  //   for (let i = 0; i < arr.length; i++) {
+  //     invertedArr.push(-arr[i]);
+  //   }
+  // return arr.map(num => -num);
+  //     return arr;
+  //   }
+  //   return invertedArr;
+}
+console.log(invertArray([1, 2, 3, 4, 5])); //➞ [-1, -2, -3, -4, -5]
+console.log(invertArray([1, -2, 3, -4, 5])); //➞ [-1, 2, -3, 4, -5]
+console.log(invertArray([])); //➞ []
