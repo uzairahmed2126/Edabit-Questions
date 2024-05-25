518492b952a93ef90db895b9386c06084b653bac
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-05-25 14:59:06 +0530

    add one with multiple ways

---
+++ very- easy again.js/3.js
@@ -0,0 +1,29 @@
+// Return the Next Number from the Integer Passed
+// Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+
+// Examples
+function addition(number) {
+    number++;
+    return number;
+  //   return ++number;
+  //   return 1 + number;
+  //   return +true + number; // 1+number
+  //   return +!false + number; //1+number
+  //   return +!null + number; //1+number
+  //   return +!![undefined] + number; //1+number
+  //   return +!![] + number; //1+number
+  //   return +!NaN + number; //1+number
+  //   return +!![""] + number; //1+number
+  //   return +"1" + number;
+  //   return +!![null] + number;
+  //   return +!!Infinity + number;
+  //   return +!![false] + number;
+  //   return +!![true] + number;
+}
+console.log(addition(0)); //➞ 1
+console.log(addition(9)); //➞ 10
+console.log(addition(-3)); //➞ -2
+// Notes
+// Don't forget to return the result.
+// If you get stuck on a challenge, find help in the Resources tab.
+// If you're really stuck, unlock solutions in the Solutions tab.
