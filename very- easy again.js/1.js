f0d70de60d6c7271db6375d44f837d773afd2a44
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-05-25 14:38:54 +0530

    addition

--- very- easy again.js/1.js
+++ very- easy again.js/1.js
@@ -14,7 +14,9 @@ function addition(num1, num2) {
   //   return Number(num1 + +num2);
   //   return num1 + (1 * num2)
   //   return num1 + num2 / 1;
-  return num1 + Number(num2);
+  //   return num1 + Number(num2);
+  [num1, num2] = [num1, +num2];
+  return num1 + num2;
 }
 console.log(addition(3, 2)); //➞ 5
 
