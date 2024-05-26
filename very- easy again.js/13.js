c5b3b29f4623cae6a6605a169d6b7cd9ea448e4f
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-05-26 18:31:38 +0530

    is the number less than or equal to zero

---
+++ very- easy again.js/13.js
@@ -0,0 +1,14 @@
+// Is the Number Less than or Equal to Zero?
+// Create a function that takes a number as its only argument and returns true if it's less than or equal to zero, otherwise return false.
+
+// Examples
+function lessThanOrEqualToZero(num) {
+  return num <= 0;
+}
+console.log(lessThanOrEqualToZero(5)); //➞ false
+console.log(lessThanOrEqualToZero(0)); //➞ true
+console.log(lessThanOrEqualToZero(-2)); //➞ true
+// Notes
+// Don't forget to return the result.
+// If you get stuck on a challenge, find help in the Resources tab.
+// If you're really stuck, unlock solutions in the Solutions tab.
