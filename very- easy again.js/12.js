0bf58a51dd89b0bb721bcfe96ae3a867308c07c0
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-05-26 18:28:22 +0530

    correct the mistake

---
+++ very- easy again.js/12.js
@@ -0,0 +1,17 @@
+// Correct the Mistakes
+// Fix the code in the code tab to pass this challenge (only syntax errors). Look at the examples below to get an idea of what the function should do.
+
+// Examples
+function squared(num) {
+    return num * num;
+  //   return Math.pow(num, 2);
+  //   return num ** 2;
+}
+console.log(squared(5)); //➞ 25
+
+console.log(squared(9)); //➞ 81
+
+console.log(squared(100)); //➞ 10000
+// Notes
+// READ EVERY WORD CAREFULLY, CHARACTER BY CHARACTER!
+// Don't overthink this challenge; it's not supposed to be hard.
