7525cc9aa1dde8a9bf9abc118ffa27f8de63995d
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-05-28 00:12:46 +0530

    fix the expression

---
+++ very- easy again.js/25.js
@@ -0,0 +1,12 @@
+// Fix the Expression
+// Fix the code in the Code tab so the function returns true if and only if x is equal to 7.
+
+// Examples
+function isSeven(num) {
+  return num === 7;
+}
+console.log(isSeven(4)); //➞ false
+console.log(isSeven(9)); //➞ false
+console.log(isSeven(7)); //➞ true
+// Notes
+// The bug can be hard to find, so look closely!
