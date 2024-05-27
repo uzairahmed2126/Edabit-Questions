a4c1855ff9266cf88e30939b0f02909251537e2c
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-05-28 00:32:19 +0530

    make pair

---
+++ very- easy again.js/28.js
@@ -0,0 +1,12 @@
+// Pair Management
+// Given two arguments, return an array which contains these two arguments.
+
+// Examples
+function makePair(...value) {
+  return value;
+}
+console.log(makePair(1, 2)); //➞ [1, 2]
+console.log(makePair(51, 21)); //➞ [51, 21]
+console.log(makePair(512124, 215)); //➞ [512124, 215]
+// Notes
+// N/A
