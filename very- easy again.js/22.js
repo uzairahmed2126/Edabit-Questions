91df7d87f2199c66672e2940ddd5ba887697040f
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-05-27 23:44:17 +0530

    are the numbers equal

---
+++ very- easy again.js/22.js
@@ -0,0 +1,14 @@
+// Are the Numbers Equal?
+// Create a function that returns true when num1 is equal to num2; otherwise return false.
+
+// Examples
+function isSameNum(num1, num2) {
+  return num1 === num2;
+}
+console.log(isSameNum(4, 8)); //➞ false
+
+console.log(isSameNum(2, 2)); //➞  true
+
+console.log(isSameNum(2, "2")); //➞ false
+// Notes
+// Don't forget to return the result.
