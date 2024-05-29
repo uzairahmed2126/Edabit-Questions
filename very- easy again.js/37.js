f38e06e4aa16d7c34a27ee55b980b3950051dee8
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-05-29 15:41:20 +0530

    find index

--- very- easy again.js/37.js
+++ very- easy again.js/37.js
@@ -8,6 +8,7 @@ function valueAt(arr, index) {
 }
 console.log(valueAt([1, 2, 3, 4, 5, 6], 10 / 2)); //➞ 6
 console.log(valueAt([1, 2, 3, 4, 5, 6], 8.0 / 2)); //➞ 5
+
 console.log(valueAt([1, 2, 3, 4], 6.535355314 / 2)); //➞ 4
 // Notes
 // Math.floor() can be helpful.
