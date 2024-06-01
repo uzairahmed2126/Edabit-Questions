6e90600632d3d99ef5a72238f4ca5c163693e389
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-06-02 00:27:00 +0530

    return types

--- very- easy again.js/47.js
+++ very- easy again.js/47.js
@@ -2,11 +2,21 @@
 // Create a function that takes an array and returns the types of values (data types) in a new array.
 
 // Examples
-console.log(arrayValuesTypes([1, 2, "null", []]))
+function arrayValuesTypes(arr) {
+  //   let result = [];
+  //   for (let i = 0; i < arr.length; i++) {
+  //     result.push(typeof arr[i]);
+  //   }
+  //   return result;
+    return arr.map((elements) => typeof elements);
+}
+console.log(arrayValuesTypes([1, 2, "null", []]));
 //➞ ["number", "number", "string", "object"]
-console.log(arrayValuesTypes(["214", true, false, 2, 2.15, [], null]))
+console.log(arrayValuesTypes(["214", true, false, 2, 2.15, [], null]));
 //➞ ["string", "boolean", "boolean", "number", "number", "object", "object"]
-console.log(arrayValuesTypes([21.1, "float", "array", ["I am array"], null, true, 214]))
+console.log(
+  arrayValuesTypes([21.1, "float", "array", ["I am array"], null, true, 214])
+);
 //➞ ["number", "string", "string", "object", "object", "boolean", "number"]
 // Notes
 // Check the Resources tab for help if needed.
