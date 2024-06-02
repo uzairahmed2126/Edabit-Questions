635b304d5d8406597dd0936277aaa26b1a40ade6
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-06-02 20:24:39 +0530

    spaces between each character

---
+++ very- easy again.js/57.js
@@ -0,0 +1,17 @@
+// Spaces Between Each Character
+// Create a function that takes a string and returns a string with spaces in between all of the characters.
+
+// Examples
+function spaceMeOut(str) {
+  // let result = "";
+  // for (let i = 0; i < str.length; i++) {
+  //   result += str[i] + " ";
+  // }
+  // return result;
+  return str.split("").join(" ");
+}
+console.log(spaceMeOut("space")); //➞ "s p a c e"
+console.log(spaceMeOut("far out")); //➞ "f a r   o u t"
+console.log(spaceMeOut("elongated musk")); //➞ "e l o n g a t e d   m u s k"
+// Notes
+// Treat a space as its own character (i.e. leave three spaces between words).
