335c93dab6c5f4d5467543fbd71c03f4c5d81025
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-06-16 16:07:05 +0530

    find the smallest and biggest number

--- easy/1.Find the Smallest and Biggest Numbers.js
+++ easy/1.Find the Smallest and Biggest Numbers.js
@@ -1,20 +1,22 @@
+// Find the Smallest and Biggest Numbers
+// Create a function that takes an array of numbers and return both the minimum and maximum numbers, in that order.
+
+// Examples
 function minMax(arr) {
-    // return [Math.min(...arr),Math.max(...arr)]
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < arr.length - i - 1; j++) {
-          if (arr[j] > arr[j + 1]) {
-            let temp = arr[j];
-            arr[j] = arr[j + 1];
-            arr[j + 1] = temp;
-          }
-        }
+  // return [Math.min(...arr), Math.max(...arr)];
+  // arr.sort((a, b) => a - b);
+  // return [arr[0], arr[arr.length - 1]];
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
       }
-    
-      // After sorting, return [min, max]
-      return [arr[0], arr[arr.length - 1]];
+    }
+  }
+  return [arr[0], arr[arr.length - 1]];
 }
 console.log(minMax([1, 2, 3, 4, 5])); //➞ [1, 5]
-
 console.log(minMax([2334454, 5])); //➞ [5, 2334454]
-
 console.log(minMax([1])); //➞ [1, 1]
+// Notes
+// All test arrays will have at least one element and are valid.
