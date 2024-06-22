7abf06dfdf7e088f6bee610a43336b3c46dd319c
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-06-22 15:18:14 +0530

    sum of cubes

---
+++ easy again/8.js
@@ -0,0 +1,18 @@
+// Sum of Cubes
+// Create a function that takes in an array of numbers and returns the sum of its cubes.
+
+// Examples
+function sumOfCubes(arr) {
+  let result = 0;
+  for (let i = 0; i < arr.length; i++) {
+    result += arr[i] * arr[i] * arr[i];
+  }
+  return result;
+}
+console.log(sumOfCubes([1, 5, 9])); //➞ 855
+// Since 1^3 + 5^3 + 9^3 = 1 + 125 + 729 = 855
+console.log(sumOfCubes([3, 4, 5])); //➞ 216
+console.log(sumOfCubes([2])); //➞ 8
+console.log(sumOfCubes([])); //➞ 0
+// Notes
+// If given an empty array, return 0.
