51d5289bacadbd055dfb2d384cbe5c34f4e58e63
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-06-27 18:12:43 +0530

    find the index

---
+++ string/very-easy/18.js
@@ -0,0 +1,19 @@
+// Find the Index
+// Create a function that takes an array and a string as arguments and returns the index of the string.
+
+// Examples
+function findIndex(arr, val) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === val) {
+      return i;
+    }
+  }
+  // return arr.indexOf(val)
+}
+console.log(findIndex(["hi", "edabit", "fgh", "abc"], "fgh")); //➞ 2
+console.log(findIndex(["Red", "blue", "Blue", "Green"], "blue")); //➞ 1
+console.log(findIndex(["a", "g", "y", "d"], "d")); //➞ 3
+console.log(findIndex(["Pineapple", "Orange", "Grape", "Apple"], "Pineapple")); //➞ 0
+// Notes
+// Don't forget to return the result.
+// If you are stuck, find help in the Resources tab.
