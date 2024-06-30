251d06b9c9a3716815d5f01e35149c4863f08e27
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-06-30 18:43:55 +0530

    case insensitive comparison

--- string/very-easy/34.js
+++ string/very-easy/34.js
@@ -3,8 +3,17 @@
 
 // Examples
 function match(str1, str2) {
-  let lowerString = str2.toLowerCase();
-  return str1.includes(lowerString);
+  //   let lowerString = str2.toLowerCase();
+  //   return str1.includes(lowerString);
+  if (str1.length !== str2.length) {
+    return false;
+  }
+  for (let i = 0; i < str1.length; i++) {
+    if (str1.toLowerCase().charCodeAt(i) === str2.toLowerCase().charCodeAt(i)) {
+      return true;
+    }
+  }
+  return false;
 }
 console.log(match("hello", "hELLo")); //➞ true
 console.log(match("motive", "emotive")); //➞ false
