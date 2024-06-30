9d3165515fbc099b53c97e2fb9a0814f80aa0643
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-06-30 17:43:27 +0530

    char-to-ASCII

---
+++ string/very-easy/32.js
@@ -0,0 +1,19 @@
+// Char-to-ASCII
+// Create a function that returns the ASCII value of the passed in character.
+
+// Examples
+function ctoa(str) {
+  // return str.charCodeAt();
+  // return str.codePointAt()
+}
+console.log(ctoa("A")); //➞ 65
+
+console.log(ctoa("m")); //➞ 109
+
+console.log(ctoa("[")); //➞ 91
+
+console.log(ctoa("\\")); //➞ 92
+// Notes
+// Don't forget to return the result.
+// If you get stuck on a challenge, find help in the Resources tab.
+// If you're really stuck, unlock solutions in the Solutions tab.
