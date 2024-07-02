36a6a517fb8d5ecf741e06604416eed8da1659c9
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-07-03 01:25:27 +0530

    filter strings from array

---
+++ string/very-easy/51.js
@@ -0,0 +1,25 @@
+// Filter Strings from Array
+// Create a function that takes an array of strings and numbers, and filters out the array so that it returns an array of integers only.
+
+// Examples
+function filterArray(arr) {
+  //   let result = [];
+  //   for (let i = 0; i < arr.length; i++) {
+  //     if (typeof arr[i] !== "string") {
+  //       result.push(arr[i]);
+  //     }
+  //   }
+  //   return result;
+  //   return arr.filter((item) => typeof item !== "string");
+  return arr.reduce((acc, cur) => {
+    if (typeof cur !== "string") {
+      acc.push(cur);
+    }
+    return acc;
+  }, []);
+}
+console.log(filterArray([1, 2, 3, "a", "b", 4])); //➞ [1, 2, 3, 4]
+console.log(filterArray(["A", 0, "Edabit", 1729, "Python", "1729"])); //➞ [0, 1729]
+console.log(filterArray(["Nothing", "here"])); //➞ []
+// Notes
+// N/A
