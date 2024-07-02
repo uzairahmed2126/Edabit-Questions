a7c86a8db0925e5ee0545ccfa599985ab3a633d9
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-07-02 13:31:17 +0530

    the study fo wumbology

---
+++ string/very-easy/48.js
@@ -0,0 +1,25 @@
+// The Study of Wumbology
+// Create a function that flips M's to W's (all uppercase).
+
+// Examples
+function wumbo(str) {
+  // let result = "";
+  // let split = str.split("");
+  // for (let i = 0; i < split.length; i++) {
+  //   if (split[i] === "M") {
+  //     split[i] = "W";
+  //   }
+  //   result += split[i];
+  // }
+  // return result;
+  // let result = "";
+  // for (let i = 0; i < str.length; i++) {
+  //   result += str[i].replace(/^[M]/g, "W");
+  // }
+  // return result;
+}
+console.log(wumbo("I LOVE MAKING CHALLENGES")); //➞ "I LOVE WAKING CHALLENGES"
+console.log(wumbo("MEET ME IN WARSAW")); //➞ "WEET WE IN WARSAW"
+console.log(wumbo("WUMBOLOGY")); //➞ "WUWBOLOGY"
+// Notes
+// N/A
