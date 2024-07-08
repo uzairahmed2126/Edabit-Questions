8cf5c6951be9422997ea0961a3337e1b37b77e93
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-07-09 00:53:02 +0530

    the forbidden letter

---
+++ string/very-easy/59.js
@@ -0,0 +1,24 @@
+// The Forbidden Letter
+// Given a letter and an array of words, return whether the letter does not appear in any of the words.
+
+// Examples
+function forbiddenLetter(letter, arr) {
+  //   for (let i = 0; i < arr.length; i++) {
+  //     if (arr[i][i] === letter) {
+  //       return false;
+  //     }
+  //   }
+  //   return true;
+  return arr.reduce((acc, cur) => {
+    if (cur.includes(letter)) {
+      acc = false;
+    }
+    return acc;
+  }, true);
+}
+console.log(forbiddenLetter("r", ["rock", "paper", "scissors"])); //➞ false
+console.log(forbiddenLetter("a", ["spoon", "fork", "knife"])); //➞ true
+console.log(forbiddenLetter("m", [])); //➞ true
+// Notes
+// All inputs given will be in lowercase.
+// You will always be given a forbidden letter, but there may be empty arrays.
