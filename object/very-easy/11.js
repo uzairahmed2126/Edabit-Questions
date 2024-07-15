7814966b6b78e47d42feae7417c2fb61f90420f4
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-07-16 04:10:26 +0530

    shapes with n sides

---
+++ object/very-easy/11.js
@@ -0,0 +1,30 @@
+// Shapes With N Sides
+// Create a function that takes a whole number as input and returns the shape with that number's amount of sides. Here are the expected outputs to get from these inputs.
+
+// Inputs	Outputs
+
+// Examples
+function nSidedShape(num) {
+  let obj = {
+    1: "circle",
+    2: "semi-circle",
+    3: "triangle",
+    4: "square",
+    5: "pentagon",
+    6: "hexagon",
+    7: "heptagon",
+    8: "octagon",
+    9: "nonagon",
+    10: "decagon",
+  };
+  return obj[num]
+}
+console.log(nSidedShape(3)); //➞ "triangle"
+
+console.log(nSidedShape(1)); //➞ "circle"
+
+console.log(nSidedShape(9)); //➞ "nonagon"
+// Notes
+// There won't be any tests with a number below 1 or greater than 10.
+// Return the output in lowercase.
+// The challenge is intended to be completed without conditionals (it would take too long)!
