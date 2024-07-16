67b84a03df341f76f7ebaed709567ffe96666852
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-07-16 21:14:56 +0530

    destructuring objects I

---
+++ object/very-easy/21.js
@@ -0,0 +1,18 @@
+// ES6: Destructuring Objects I
+// In JavaScript, you can do basic object assignment like this:
+
+const obj = { one: 1, two: 2, three: 3 };
+
+let one = obj.one;
+let two = obj.two;
+let three = obj.three;
+console.log(one, two, three);
+// However, with ES6 you can assign the variables in a much more succinct way. Use ES6 object destructuring to assign variables one and two to obj.one and obj.two respectively.
+
+// Although you can use let, var, or const for assignment, DO NOT use these in this challenge.
+
+// Notes
+// Ignore the backticks `` (used for validation).
+// Ignore the .toString() function (used for validation).
+// Replace the commented part of the first line with proper, left-side part of the solution.
+// If you know how to use object destructuring, go ahead and complete the challenge, otherwise check the Resources tab for examples and then come back to try the challenge.
