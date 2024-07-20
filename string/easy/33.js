a551d62d68efb394e206d2629a74d7d813c18fcd
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-07-21 04:27:56 +0530

    spelling it out

---
+++ string/easy/33.js
@@ -0,0 +1,20 @@
+// Spelling it Out
+// Create a function which takes in a word and spells it out, by consecutively adding letters until the full word is completed.
+
+// Examples
+function spelling(str) {
+  //   let arr = [];
+  //   let string = "";
+  //   for (let i = 0; i < str.length; i++) {
+  //     string += str[i];
+  //     arr.push(string);
+  //   }
+  //   return arr;
+}
+console.log(spelling("bee")); //➞ ["b", "be", "bee"]
+
+console.log(spelling("happy")); //➞ ["h", "ha", "hap", "happ", "happy"]
+
+console.log(spelling("eagerly")); //➞ ["e", "ea", "eag", "eage", "eager", "eagerl", "eagerly"]
+// Notes
+// N/A
