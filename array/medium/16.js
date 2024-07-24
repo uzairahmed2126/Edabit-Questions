30371fd3ee735969a3c0eee81fa42dcf8ad1f888
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-07-24 20:49:59 +0530

    special arrays

---
+++ array/medium/16.js
@@ -0,0 +1,32 @@
+// Special Arrays
+// An array is special if every even index contains an even number and every odd index contains an odd number. Create a function that returns true if an array is special, and false otherwise.
+
+// Examples
+function isSpecialArray(arr) {
+  let res = true;
+  for (let i = 0; i < arr.length; i++) {
+    if (
+      (i % 2 === 0 && arr[i] % 2 !== 0) ||
+      (i % 2 !== 0 && arr[i] % 2 === 0)
+    ) {
+      res = false;
+    }
+  }
+  return res;
+}
+// function isSpecialArray(arr) {
+//   return arr.every(
+//     (item, index) =>
+//       (index % 2 === 0 && item % 2 === 0) || (index % 2 !== 0 && item % 2 !== 0)
+//   );
+// }
+console.log(isSpecialArray([2, 7, 4, 9, 6, 1, 6, 3])); //➞ true
+// Even indices: [2, 4, 6, 6]; Odd indices: [7, 9, 1, 3]
+
+console.log(isSpecialArray([2, 7, 9, 1, 6, 1, 6, 3])); //➞ false
+// Index 2 has an odd number 9.
+
+console.log(isSpecialArray([2, 7, 8, 8, 6, 1, 6, 3])); //➞ false
+// Index 3 has an even number 8.
+// Notes
+// N/A
