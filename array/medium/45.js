4e0feee7a52668cb11af98491ad8ccbb0e99b80e
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-07-28 18:48:03 +0530

    transform upvotes

---
+++ array/medium/45.js
@@ -0,0 +1,23 @@
+// Transform Upvotes
+// Create a function that transforms a string of upvote counts into an array of numbers. Each k represents a thousand.
+
+// Examples
+function transformUpvotes(str) {
+  let splited = str.split(" ");
+  return splited.map((item) => item.replace("k", "") * 1000);
+  //   let result = [];
+  //   for (let i = 0; i < splited.length; i++) {
+  //     const num = parseFloat(splited[i]);
+  //     if (splited[i].includes("k")) {
+  //       result.push(num * 1000);
+  //     } else {
+  //       result.push(num);
+  //     }
+  //   }
+  //   return result;
+}
+console.log(transformUpvotes("6.8k 13.5k")); //➞ [6800, 13500]
+console.log(transformUpvotes("5.5k 8.9k 32")); //➞ [5500, 8900, 32]
+console.log(transformUpvotes("20.3k 3.8k 7.7k 992")); //➞ [20300, 3800, 7700, 992]
+// Notes
+// Return the upvotes as an array.
