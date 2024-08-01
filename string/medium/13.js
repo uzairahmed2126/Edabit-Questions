0da3d24571a0f139f1263873fa365acaf523f85d
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-08-01 22:13:37 +0530

    capitalize the first letter of each word

--- string/medium/13.js
+++ string/medium/13.js
@@ -5,10 +5,16 @@
 function makeTitle(str) {
   let result = [];
   let arr = str.split(" ");
-  return arr.reduce((acc, cur) => {
-    acc.push(cur[0].toUpperCase() + cur.slice(1));
-    return acc;
-  }, []);
+  for (let i = 0; i < arr.length; i++) {
+    const firstChar = arr[i][0];
+    const word = arr[i];
+    result.push(word.replace(firstChar, firstChar.toUpperCase()));
+  }
+  return result.join(" ");
+  //   return arr.reduce((acc, cur) => {
+  //     acc.push(cur[0].toUpperCase() + cur.slice(1));
+  //     return acc;
+  //   }, []);
   //   return arr.map((item) => item[0].toUpperCase() + item.slice(1));
   //   for (let i = 0; i < arr.length; i++) {
   //     const firstChar = arr[i][0];
