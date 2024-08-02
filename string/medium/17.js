76a1801f316e0469c00735227efe846812c65664
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-08-03 00:50:21 +0530

    find the characters counterpart char code

---
+++ string/medium/17.js
@@ -0,0 +1,22 @@
+// Find the Characters Counterpart Char Code
+// Create a function that takes a single character as an argument and returns the char code of its lowercased / uppercased counterpart.
+
+// Examples
+// Given that:
+//   - "A" char code is: 65
+//   - "a" char code is: 97
+function counterpartCharCode(char) {
+  if (char.toLowerCase() !== char.toUpperCase()) {
+    if (char === char.toLowerCase()) {
+      return char.toUpperCase().charCodeAt();
+    } else {
+      return char.toLowerCase().charCodeAt();
+    }
+  }
+}
+console.log(counterpartCharCode("A")); //➞ 97
+
+console.log(counterpartCharCode("a")); //➞ 65
+// Notes
+// The argument will always be a single character.
+// Not all inputs will have a counterpart (e.g. numbers), in which case return the input's char code.
