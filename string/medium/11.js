5ba1071eae5115e04545e4b81fa44c4360a9a1d1
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-08-02 23:10:27 +0530

    multiplying numbers in a string

--- string/medium/11.js
+++ string/medium/11.js
@@ -1,59 +1,25 @@
-// Barbecue Skewers
-// You are in charge of the barbecue grill. A vegetarian skewer is a skewer that has only vegetables (-o). A non-vegetarian skewer is a skewer with at least one piece of meat (-x).
-
-// For example, the grill below has 4 non-vegetarian skewers and 1 vegetarian skewer (the one in the middle).
-
-// ["--xo--x--ox--",
-// "--xx--x--xx--",
-// "--oo--o--oo--",      <<< vegetarian skewer
-// "--xx--x--ox--",
-// "--xx--x--ox--"]
-// Given a BBQ grill, write a function that returns [# vegetarian skewers, # non-vegetarian skewers]. For example above, the function should return [1, 4].
+// Multiplying Numbers in a String
+// Given a string of numbers separated by a comma and space, return the product of the numbers.
 
 // Examples
-function bbqSkewers(skewer) {
-  return skewer.reduce(
-    (acc, cur) => {
-      let splited = cur.split("");
-      if (splited.includes("o") && !splited.includes("x")) {
-        acc[0]++;
-      } else {
-        acc[1]++;
-      }
-      return acc;
-    },
-    [0, 0]
-  );
-  //   let vegetarianSkewer = 0;
-  //   let nonVegetarianSkewer = 0;
-  //   for (let i = 0; i < skewer.length; i++) {
-  //     let splited = skewer[i].split("");
-  //     if (splited.includes("o") && !splited.includes("x")) {
-  //       vegetarianSkewer++;
-  //     } else {
-  //       nonVegetarianSkewer++;
-  //     }
-  //   }
-  //   return [vegetarianSkewer, nonVegetarianSkewer];
+function multiplyNums(str) {
+  let arr = str.split(",");
+  return arr.reduce((acc, cur) => {
+    acc *= cur;
+    return acc;
+  }, 1);
+  // let result  = 1;
+  // for (let i = 0; i < arr.length; i++) {
+  //   result *= arr[i];
+  // }
+  // return result;
 }
-console.log(
-  bbqSkewers([
-    "--oooo-ooo--",
-    "--xx--x--xx--",
-    "--o---o--oo--",
-    "--xx--x--ox--",
-    "--xx--x--ox--",
-  ])
-); //➞ [2, 3]
+console.log(multiplyNums("2, 3")); //➞ 6
 
-console.log(
-  bbqSkewers([
-    "--oooo-ooo--",
-    "--xxxxxxxx--",
-    "--o---",
-    "-o-----o---x--",
-    "--o---o-----",
-  ])
-); //➞ [3, 2]
+console.log(multiplyNums("1, 2, 3, 4")); //➞ 24
+
+console.log(multiplyNums("54, 75, 453, 0")); //➞ 0
+
+console.log(multiplyNums("10, -2")); //➞ -20
 // Notes
-// N/A
+// Bonus: Try to complete this challenge in one line!
