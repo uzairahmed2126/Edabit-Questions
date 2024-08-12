c9b6ff7dae61616a0233e24e12d14a88091f8e7b
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-08-12 17:20:47 +0530

    make a circle with oop

---
+++ object/medium/4.js
@@ -0,0 +1,27 @@
+// Make a Circle with OOP
+// Your task is to create a Circle constructor that creates a circle with a radius provided by an argument. The circles constructed must have two methods getArea() (PI*r^2) and getPerimeter() (2*PI*r) which give both respective areas and perimeter (circumference).
+
+// For help with this class, I have provided you with a Rectangle constructor which you can use as a base example.
+
+// Examples
+class Circle {
+  constructor(radius) {
+    this.radius = radius;
+  }
+  getArea() {
+    return Math.PI * Math.pow(this.radius, 2);
+  }
+  getPerimeter() {
+    return 2 * Math.PI * this.radius;
+  }
+}
+let circy1 = new Circle(11);
+console.log(circy1.getArea());
+
+// Should return 380.132711084365
+let circy2 = new Circle(4.44);
+console.log(circy2.getPerimeter());
+
+// Should return 27.897342763877365
+// Notes
+// Don't worry about floating point precision - I've factored in the chance that your answer may be more or less accurate than mine. This is more of a tutorial than a challenge so the topic covered may be considered advanced, yet the challenge is more simple - so if this challenge gets labelled as easy, don't worry too much.
