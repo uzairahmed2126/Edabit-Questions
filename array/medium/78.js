c9546c78d873309560511b4d4b84de1ecd7874db
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-09-03 19:36:45 +0530

    flatten the curve

--- array/medium/78.js
+++ array/medium/78.js
@@ -2,17 +2,26 @@
 // Given an array of integers, replace every number with the mean of all numbers.
 
 // Examples
+// function flattenCurve(arr) {
+//   let result = [];
+//   let index = Math.trunc(arr.length / 2);
+//   for (let i = 0; i < arr.length; i++) {
+//     if (index !== i) {
+//       result.push(arr[index]);
+//     }else {
+//         result.push(arr[i])
+//     }
+//   }
+//   return result;
+// }
 function flattenCurve(arr) {
-  let result = [];
   let index = Math.trunc(arr.length / 2);
-  for (let i = 0; i < arr.length; i++) {
-    if (index !== i) {
-      result.push(arr[index]);
-    }else {
-        result.push(arr[i])
+  return arr.map((item, ind) => {
+    if (ind !== index) {
+      return arr[index];
     }
-  }
-  return result;
+    return item;
+  });
 }
 console.log(flattenCurve([1, 2, 3, 4, 5])); //➞ [3, 3, 3, 3, 3]
 console.log(flattenCurve([0, 0, 0, 2, 7, 3])); //➞ [2, 2, 2, 2, 2, 2]
