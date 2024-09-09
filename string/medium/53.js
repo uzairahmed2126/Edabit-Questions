80c8f7a40dee2068833e5053a0cb486332f6c40c
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-09-09 19:51:34 +0530

    clear the fog

--- string/medium/53.js
+++ string/medium/53.js
@@ -1,7 +1,7 @@
-// Clear the Fog
-// Create a function which returns the word in the string, but with all the fog letters removed. However, if the string is clear from fog, return "It's a clear day!".
+// // Clear the Fog
+// // Create a function which returns the word in the string, but with all the fog letters removed. However, if the string is clear from fog, return "It's a clear day!".
 
-// Examples
+// // Examples
 function clearFog(str) {
   if (!str.includes("fog")) {
     return "It's a clear day!";
@@ -18,6 +18,6 @@ function clearFog(str) {
 console.log(clearFog("sky")); // "It's a clear day!"
 console.log(clearFog("fogfogFFfoooofftogffreogffesGgfOogfog")); // "trees"
 console.log(clearFog("fogFogFogffoObirdsanffodthebffoeffoesGGGfOgFog")); // "birdsandthebees"
-// Notes
-// Hidden words won't include the letters f, o or g.
-// Hidden words are always in lowercase.
+// // Notes
+// // Hidden words won't include the letters f, o or g.
+// // Hidden words are always in lowercase.
