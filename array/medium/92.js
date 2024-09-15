e7a2da233325ea1fe34615c67937cd093d5fd455
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-09-16 01:27:30 +0530

    numbers to objects

--- array/medium/92.js
+++ array/medium/92.js
@@ -8,8 +8,24 @@
 // The value will be the corresponding character code converted to a string (check ASCII table).
 // Return an array of the resulting objects.
 // Examples
+// function numObj(arr) {
+//   let result = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     let obj = {};
+//     let char = String.fromCharCode(arr[i]);
+//     obj[arr[i]] = char;
+//     result.push(obj);
+//   }
+//   return result;
+// }
 function numObj(arr) {
-    return arr
+  return arr.reduce((acc, cur) => {
+    let obj ={};
+    let char = String.fromCharCode(cur);
+    obj[cur] = char;
+    acc.push(obj);
+    return acc;
+  }, []);
 }
 console.log(numObj([118, 117, 120])); //➞ [{'118':'v'}, {'117':'u'}, {'120':'x'}]
 console.log(numObj([101, 121, 110, 113, 103])); //➞ [{'101':'e'}, {'121':'y'}, {'110':'n'}, {'113':'q'}, {'103':'g'}]
