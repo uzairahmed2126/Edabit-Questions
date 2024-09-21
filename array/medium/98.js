6d3ddaad388d14382ca7e0e9dd501b401f6bdb65
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-09-21 18:37:21 +0530

    count the towers

--- array/medium/98.js
+++ array/medium/98.js
@@ -3,23 +3,45 @@
 
 // Examples
 // 1.count biggest tower
-// 2.do it with object
-function count_towers(arr) {
-  let a;
-  for (let i = 0; i < arr.length; i++) {
-    a = arr[i]
-      .reduce((acc, cur) => {
-        if (!acc.includes("##")) {
-          acc.push(cur);
-        }
-        return acc;
-      }, [])
-      .join("")
-      .split(" ")
-      .join("");
+// function count_towers(arr) {
+//   let obj = {};
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr[i][0].length; j = j + 2) {
+//       if (arr[i][0][j] !== " ") {
+//         if (obj[j] === undefined) obj[j] = 1;
+//         else obj[j]++;
+//       }
+//     }
+//   }
+//   let val = 0;
+//   for (let key in obj) {
+//     if (obj[key] > val) val = obj[key];
+//   }
+//   return val;
+// }
+// function count_towers(arr) {
+//   let a;
+//   for (let i = 0; i < arr.length; i++) {
+//     a = arr[i]
+//       .reduce((acc, cur) => {
+//         if (!acc.includes("##")) {
+//           acc.push(cur);
+//         }
+//         return acc;
+//       }, [])
+//       .join("")
+//       .split(" ")
+//       .join("");
+//   }
+//   let total = a.length / 2;
+//   return total;
+// }
+function count_towers(towers) {
+  let arr;
+  for (let i = 0; i < towers.length; i++) {
+    arr = towers[i][towers[i].length - 1];
   }
-  let total = a.length / 2;
-  return total;
+  return arr.split("  ").length;
 }
 console.log(
   count_towers([
