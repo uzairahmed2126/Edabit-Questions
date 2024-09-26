477f2a7db3a0ea2b2c7aea98371343c706343a17
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-09-26 23:58:12 +0530

    .

---
+++ string/medium/63.js
@@ -0,0 +1,14 @@
+// Fix the Spacing
+// Additional spaces have been added to a sentence. Return the correct sentence by removing them. All words should be separated by one space, and there should be no spaces at the beginning or end of the sentence.
+
+// Examples
+console.log(correctSpacing("The film   starts       at      midnight. "))
+//➞ "The film starts at midnight."
+
+console.log(correctSpacing("The     waves were crashing  on the     shore.   "))
+//➞ "The waves were crashing on the shore."
+
+console.log(correctSpacing(" Always look on    the bright   side of  life."))
+//➞ "Always look on the bright side of life."
+// Notes
+// N/A
