8efda096ad2c673cb85e390b18d9cc49ebf97639
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-10-15 12:13:42 +0530

    array multiplier ;

--- array/medium/106.js
+++ array/medium/106.js
@@ -2,17 +2,28 @@
 // Create a function that takes an array as an argument and returns a new nested array for each element in the original array. The nested array must be filled with the corresponding element (string or number) in the original array and each nested array has the same amount of elements as the original array.
 
 // Examples
+// function multiply(arr) {
+//   let result = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     let innerArr = [];
+//     for (let j = 0; j < arr.length; j++) {
+//       let num = arr[i];
+//       innerArr.push(num);
+//     }
+//     result.push(innerArr);
+//   }
+//   return result;
+// }
 function multiply(arr) {
-  let result = [];
-  for (let i = 0; i < arr.length; i++) {
-    let innerArr = [];
-    for (let j = 0; j < arr.length; j++) {
-      let num = arr[i];
-      innerArr.push(num);
-    }
-    result.push(innerArr);
-  }
-  return result;
+  return arr.reduce((acc, cur) => {
+    let str = String(cur);
+    let repeat = arr.length;
+    let multiply = str.repeat(repeat);
+    let splited = multiply.split("");
+    let mapped = splited.map((item) => (+item ? +item : item));
+    acc.push(mapped);
+    return acc;
+  }, []);
 }
 console.log(multiply([4, 5])); //➞ [[4, 4], [5, 5]]
 console.log(multiply(["*", "%", "$"])); //➞ [["*", "*", "*"], ["%", "%", "%"], ["$", "$", "$"]]
