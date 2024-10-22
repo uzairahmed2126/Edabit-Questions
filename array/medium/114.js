730e20fc432dcc1f8f8ce5b512a4821f983bbbd6
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-10-22 20:02:04 +0530

    cumulative array sum

--- array/medium/114.js
+++ array/medium/114.js
@@ -3,14 +3,22 @@
 
 // Examples
 function cumulativeSum(arr) {
-  let result = [arr[0]];
+  let result = [];
   let sum = 0;
-  for (let i = 1; i < arr.length; i++) {
-    sum += arr[i - 1];
-    result.push(sum + arr[i]);
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+    result.push(sum);
   }
   return result;
 }
+// function cumulativeSum(arr) {
+//   let sum = 0;
+//   return arr.reduce((acc, cur) => {
+//     sum+=cur
+//     acc.push(sum);
+//     return acc;
+//   }, []);
+// }
 console.log(cumulativeSum([1, 2, 3])); //➞ [1, 3, 6]
 console.log(cumulativeSum([1, -2, 3])); //➞ [1, -1, 2]
 console.log(cumulativeSum([3, 3, -2, 408, 3, 3])); //➞ [3, 6, 4, 412, 415, 418]
